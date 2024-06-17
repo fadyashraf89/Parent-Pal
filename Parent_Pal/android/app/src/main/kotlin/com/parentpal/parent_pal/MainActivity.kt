d382c4bede9e050aa57913541aef5bfb84c8835f
Fady <fadyashraf89@gmail.com>
2024-06-18 01:55:15 +0300

    Add Login Page

---
+++ Parent_Pal/android/app/src/main/kotlin/com/parentpal/parent_pal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.parentpal.parent_pal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Parent_Pal/lib/pages/login_page.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // Text field controllers for email and password
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  Color focusedColor = Color(0xFF5571A7); // Define focused border color
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false, // Prevent the UI from resizing when the keyboard appears
+      body: Padding(
+        padding: const EdgeInsets.only(top: 60),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: ListView(
+              physics: NeverScrollableScrollPhysics(), // Disable scrolling
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        "assets/images/app-logo.png",
+                        width: 180,
+                        height: 180,
+                      ),
+                      Text("Parent Pal",
+                          style: TextStyle(
+                              fontFamily: "Pacifico",
+                              fontSize: 48,
+                              color: Color(0xFF5571A7))),
+                    ],
+                  ),
+                ),
+
+                // Email text field with frame
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: TextField(
+                    style: TextStyle(fontFamily: "Rubik"),
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(color: focusedColor, width: 2.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(color: Colors.grey, width: 2.0),
+                      ),
+                    ),
+                  ),
+                ),
+                // Password text field with frame
+                TextField(
+                  style: TextStyle(fontFamily: "Rubik"),
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: focusedColor, width: 2.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: Colors.grey, width: 2.0),
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        // Handle forgot password logic
+                      },
+                      child: Text(
+                        'Forgot password?',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF5571A7),
+                            fontFamily: "Rubik",
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0), // Add spacing between elements
+                // Login button with full width
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle login logic
+                  },
+                  child:
+                  Text('Login', style: TextStyle(fontFamily: "Pacifico", fontSize: 20)),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size(double.infinity, 50),
+                      backgroundColor: Color(0xFF5571A7)),
+                ),
+                SizedBox(height: 20.0), // Add spacing between elements
+                // Sign up section
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Don\'t have an account?',
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, fontFamily: "Rubik")),
+                    TextButton(
+                      onPressed: () {
+                        // Navigate to sign up page
+                      },
+                      child: Text('Sign Up',
+                          style: TextStyle(
+                              fontSize: 16,fontWeight: FontWeight.bold, fontFamily: "Rubik", color: Color(0xFF5571A7))),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() => runApp(LoginPage());
